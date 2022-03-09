6f6c2e6b9845b0461f87f65d56b572d33bb8c008
hemanth <reddyhemanth289@gmail.com>
2022-03-09 13:24:46 +0530

    home

---
+++ hr-1/src/test/java/phr/AppTest.java
@@ -0,0 +1,20 @@
+package phr;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
